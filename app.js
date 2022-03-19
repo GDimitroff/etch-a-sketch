e3acb76f6abc27b32eabcf5772237f0094fb4725
Gospodin Dimitrov <gospodindimitroff@gmail.com>
2022-03-19 14:06:42 +0200

    Implement dynamic background and borders color

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const grid = document.querySelector('.grid');
+const gridBackground = document.querySelector(
+  '.options-grid-background > input'
+);
+const gridBorders = document.querySelector('.options-grid-borders > input');
 const densityInput = document.querySelector('.options-grid-density > input');
 const densityLabel = document.querySelector('.options-grid-density > label');
 
+const defaultGridBackgroundColor = '#fdf4ff';
+const defaultGridBordersColor = '#D0D0D0';
+
 let cells = [];
+let gridBordersColor = '#D0D0D0';
 
 function createGrid(value = 16) {
   grid.innerHTML = '';
@@ -13,7 +21,7 @@ function createGrid(value = 16) {
 
   for (let i = 0; i < value * value; i++) {
     const cell = document.createElement('div');
-    cell.classList.add('grid-item');
+    cell.style.border = `1px solid ${gridBordersColor}`;
     cells.push(cell);
     grid.appendChild(cell);
   }
@@ -25,4 +33,16 @@ densityInput.addEventListener('change', (e) => {
   const gridValue = e.target.value;
   createGrid(gridValue);
   densityLabel.textContent = `${gridValue} x ${gridValue}`;
+});
+
+gridBackground.addEventListener('change', (e) => {
+  grid.style.backgroundColor = e.target.value;
+});
+
+gridBorders.addEventListener('change', (e) => {
+  gridBordersColor = e.target.value;
+
+  cells.forEach((cell) => {
+    cell.style.border = `1px solid ${gridBordersColor}`;
+  });
 });
